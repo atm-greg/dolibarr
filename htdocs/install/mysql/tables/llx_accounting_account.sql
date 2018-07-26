b5ba4e77a58300e6e8dac5dede399a12a00921e4
atm-greg <gregory.blemand@atm-consulting.fr>
2018-07-26 07:41:29 +0200

    intoducing error

--- htdocs/install/mysql/tables/llx_accounting_account.sql
+++ htdocs/install/mysql/tables/llx_accounting_account.sql
@@ -23,7 +23,7 @@ create table llx_accounting_account
 (
   rowid                     integer AUTO_INCREMENT PRIMARY KEY,
   entity                    integer DEFAULT 1 NOT NULL,
-  datec                     datetime,
+  datec                     datetimeaa,
   tms                       timestamp,
   fk_pcg_version            varchar(32)  NOT NULL,
   pcg_type                  varchar(20)  NOT NULL,
